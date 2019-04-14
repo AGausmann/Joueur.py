503fb15eca71a4136a51b5e073e04384d56715c7
Adam Gausmann <agausmann@fastmail.com>
2019-04-13 20:22:39 -0500

    Cleanup AI code

--- games/stardash/ai.py
+++ games/stardash/ai.py
@@ -1,10 +1,4 @@
-# This is where you build your AI for the Stardash game.
-
 from joueur.base_ai import BaseAI
-
-# <<-- Creer-Merge: imports -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-# you can add additional import(s) here
-# <<-- /Creer-Merge: imports -->>
 
 class AI(BaseAI):
     """ The AI you add and improve code inside to play Stardash. """
@@ -15,7 +9,7 @@ class AI(BaseAI):
 
         :rtype: games.stardash.game.Game
         """
-        return self._game # don't directly touch this "private" variable pls
+        return self._game
 
     @property
     def player(self):
@@ -23,7 +17,7 @@ class AI(BaseAI):
 
         :rtype: games.stardash.player.Player
         """
-        return self._player # don't directly touch this "private" variable pls
+        return self._player
 
     def get_name(self):
         """ This is the name you send to the server so your AI will control the
@@ -32,25 +26,17 @@ class AI(BaseAI):
         Returns
             str: The name of your Player.
         """
-        # <<-- Creer-Merge: get-name -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-        return "Stardash Python Player" # REPLACE THIS WITH YOUR TEAM NAME
-        # <<-- /Creer-Merge: get-name -->>
+        return "btw_i_use_void_now"
 
     def start(self):
         """ This is called once the game starts and your AI knows its player and
             game. You can initialize your AI here.
         """
-        # <<-- Creer-Merge: start -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-        # replace with your start logic
-        # <<-- /Creer-Merge: start -->>
 
     def game_updated(self):
         """ This is called every time the game's state updates, so if you are
         tracking anything you can update it here.
         """
-        # <<-- Creer-Merge: game-updated -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-        # replace with your game updated logic
-        # <<-- /Creer-Merge: game-updated -->>
 
     def end(self, won, reason):
         """ This is called when the game ends, you can clean up your data and
@@ -61,20 +47,13 @@ class AI(BaseAI):
             reason (str): The human readable string explaining why your AI won
             or lost.
         """
-        # <<-- Creer-Merge: end -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-        # replace with your end logic
-        # <<-- /Creer-Merge: end -->>
     def run_turn(self):
         """ This is called every time it is this AI.player's turn.
 
         Returns:
-            bool: Represents if you want to end your turn. True means end your turn, False means to keep your turn going and re-call this function.
+            bool: Represents if you want to end your turn. True means end your
+            turn, False means to keep your turn going and re-call this
+            function.
         """
-        # <<-- Creer-Merge: runTurn -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-        # Put your game logic here for runTurn
         return True
-        # <<-- /Creer-Merge: runTurn -->>
 
-    # <<-- Creer-Merge: functions -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-    # if you need additional functions for your AI you can add them here
-    # <<-- /Creer-Merge: functions -->>
